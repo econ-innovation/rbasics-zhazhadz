8a310c1e1e9fc5dcd946321e6228d2d7411394c8
zhazhadz <zhazhadz@mail.ustc.edu.cn>
2023-12-05 17:00:08 +0800

    第一次提交作业，已经两次手忙脚乱了

--- h1.R
+++ h1.R
@@ -1,22 +1,19 @@
-# 读取数据
 file_path <- E:/数据分析/1_1Yuanbo Zhang_aminer.cnprofileyuanbo-zhang542a4624dabfae864af93d5f.csv 
 csv_data <- read.csv(file_path, header = TRUE, stringsAsFactors = FALSE, na.strings = , fill = TRUE)
 
+# Create an empty data frame with the same number of columns as csv_data
+df <- data.frame(matrix(nrow = nrow(csv_data), ncol = ncol(csv_data)))
 
-# 创建一个空的数据框，列数与csv_data的列数相同
-df <- data.frame(matrix(nrow = nrow(csv_data), ncol(csv_data)))
-
-# 设置数据框的列名为csv_data的列名
+# Set data frame's column names to match csv_data's column names
 colnames(df) <- colnames(csv_data)
 
-# 使用一个循环逐一将csv_data的列数据赋值给df
+# Use a loop to assign column data to df
 for (i in 1:ncol(csv_data)) {
-  column_data <- csv_data[[i]]  # 选择第 i 列的数据
-  
-  # 将列数据赋值给df的相应列
-  df[[i]] <- column_data
+  df[[i]] <- csv_data[[i]]  # Assign column data directly to df
 }
 
-# 打印新的数据框
+# Print the new data frame
 print(df)
+
+# Summarize the data frame
 summary(df)
